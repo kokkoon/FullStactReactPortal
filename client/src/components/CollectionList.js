012e0eb2d5173b00467de86a77fd9429d9203677
Iqbal Novramadani <iqbal.slf@gmail.com>
2018-12-21 10:42:25 +0700

    refactor: update api url on collection list page

--- client/src/components/CollectionList.js
+++ client/src/components/CollectionList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 
+import API_URL from '../utils/api_url'
 import './CollectionList.css'
 
 class CollectionList extends Component {
@@ -14,7 +15,7 @@ class CollectionList extends Component {
 
 	componentWillMount() {
 		// get form data from backend
-		axios.get('http://localhost:5000/collection-list')
+		axios.get(`${API_URL}/collection-list`)
 			.then(res => {
 				this.setState({
 					collectionList: res.data.data
