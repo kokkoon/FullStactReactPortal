f2fb8dbab2864df9e01d52dc1571b96cac9a75ee
Iqbal Novramadani <iqbal.slf@gmail.com>
2018-12-23 10:37:38 +0700

    fix: add credits functionality

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,6 +1,8 @@
-const keys = require('../config/keys');
-const stripe = require('stripe')(keys.stripeSecretKey);
-const requireLogin = require('../middlewares/requireLogin');
+const keys = require('../config/keys')
+const stripe = require('stripe')(keys.stripeSecretKey)
+const requireLogin = require('../middlewares/requireLogin')
+const db = require('../services/mongoUtil').getDB()
+const usersCollection = db.collection('users')
 
 module.exports = app => {
   app.post('/api/stripe', requireLogin, async (req, res) => {
@@ -9,11 +11,18 @@ module.exports = app => {
       currency: 'usd',
       description: '$5 for 5 credits',
       source: req.body.id
-    });
+    })
 
     req.user.credits += 5;
-    const user = await req.user.save();
 
-    res.send(user);
-  });
-};
+    usersCollection.updateOne(
+      {googleId: req.user.googleId},
+      {$set: {credits: req.user.credits}},
+      (err, obj) => {
+      if (err) console.error(err)
+      console.log('updated document = ', obj.result.n)
+    })
+
+    res.send(req.user)
+  })
+}
