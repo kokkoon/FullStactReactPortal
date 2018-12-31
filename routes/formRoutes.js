82e82bb27236e74cf38ca37dc1714283adecd46b
Iqbal Novramadani <iqbal.slf@gmail.com>
2018-12-31 10:20:52 +0700

    feat: add approve/reject actions for external collection records

--- routes/formRoutes.js
+++ routes/formRoutes.js
@@ -288,16 +288,49 @@ module.exports = (app) => {
   })
 
   app.get('/api/external-content', (req, res) => {
-     const authorization = {
+    const authorization = {
       authorization: 'Bearer tSTsMNItVWUsFQJRJF2MtUsQ2K2O2FsIP2HtSVsLtUsP2EsItPsRPtR2etUsK2gtVUsJ2bsMtTsKtWsFtSsItWxqDsMIORRtTsNJtRsOtV'
     }
 
     request('https://app.taskngin.com/api/v1/tasks?from=2018-11-01', { headers: authorization }, (error, response, body) => {
       if (error) console.error(error)
       if (response.statusCode === 200) {
-        res.send({data: JSON.parse(body)})
+        res.send({ data: JSON.parse(body) })
       }
     })
+  })
+
+  app.patch('/api/external-content', (req, res) => {
+    const url = URL.parse(req.url, true)
+    const taskId = url.query.task_id
+    const headers = {
+      authorization: 'Bearer tSTsMNItVWUsFQJRJF2MtUsQ2K2O2FsIP2HtSVsLtUsP2EsItPsRPtR2etUsK2gtVUsJ2bsMtTsKtWsFtSsItWxqDsMIORRtTsNJtRsOtV'
+    }
+
+    const requestOptions = {
+      method: 'PATCH', 
+      uri: `https://app.taskngin.com/api/v1/tasks/${taskId}`, 
+      body: JSON.stringify(req.body), 
+      headers
+    }
+
+    request(requestOptions, (error, response, body) => {
+        if (error) console.error(error)
+        if (response.statusCode === 200) {
+          let result
+          try {
+            result = JSON.parse(body)          
+          } catch (err) {
+            if (err) result = body
+          }
+
+          res.send({ result })
+        } else {
+          res.send({ result: 'request not valid'})
+        }
+      }
+    )
+
   })
 
   // delete a document in a collection
