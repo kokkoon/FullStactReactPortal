d729207b513d74c4d363e685ad478138fad25786
Iqbal Novramadani <iqbal.slf@gmail.com>
2019-01-08 23:36:29 +0700

    chore: fix warning on mongoDB connection

--- services/mongoUtil.js
+++ services/mongoUtil.js
@@ -1,14 +1,15 @@
 const mongodb = require('mongodb')
 const keys = require('../config/keys')
-const mongoClient = new mongodb.MongoClient(keys.mongoURI);
+const mongoClient = mongodb.MongoClient
 
+const dbName = 'flowngin-dev'
 var _db
 
 module.exports = {
   connectToDB: (callback) => {
-    mongoClient.connect(err => {
-      _db = mongoClient.db('flowngin-dev')
-      return callback(err)
+    mongoClient.connect(keys.mongoURI, { useNewUrlParser: true }, (err, client) => {
+    	_db = client.db(dbName)
+    	return callback(err)
     })
   },
 
