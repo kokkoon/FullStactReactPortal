c31150d5914875d9efdae340619432cbd4a545e9
Iqbal Novramadani <iqbal.slf@gmail.com>
2019-01-08 18:06:00 +0700

    refactor: cleanup sidenav.js

--- client/src/containers/Sidenav/Sidenav.js
+++ client/src/containers/Sidenav/Sidenav.js
@@ -12,67 +12,17 @@ class Sidenav extends Component {
 
 		this.state = {
 			selectedNavItem: undefined,
-			collectionNavItemLinks: []
 		}
 	}
 
 	componentWillMount() {
-		// const pathname = window.location.pathname.slice(1)
-		// const query = window.location.search.slice(4)
-		// let selectedNavItem
 		const { 
 			setApp,
-			loadSidenavConfig,
-			// userGroupLinkAccess, 
-			// currentUserGroup, 
-			// setSidenavFromConfig,
-			// setSidenavGroupLinks,
-			// collectionNavItemLinks,
-			// setDefaultNavItem,
-			// setCollectionNavItem,
-			// loadCollectionNavItemLinks,
+			loadSidenavConfig
 		} = this.props
 
-		// const shownCollectionNavItemLinks = collectionNavItemLinks
-    
     setApp('default')
     loadSidenavConfig('default')
-		// setDefaultNavItem()
-		// setSidenavGroupLinks([])
-		// get collection links from backend
-		// setCollectionNavItem()
-		
-		// show menu based on user group authorization
-		// const shownCollectionNavItemLinks = collectionNavItemLinks ?
-		// 	collectionNavItemLinks.filter(item => 
-		// 		userGroupLinkAccess[currentUserGroup].indexOf(item.name) >= 0) : []
-
-		// const offset = collectionNavItemLinks ? shownCollectionNavItemLinks.length : 0
-
-		// if (pathname.length > 0) {
-		// 	switch (pathname) {
-		// 		case 'dashboard':
-		// 			selectedNavItem = offset
-		// 			break
-		// 		case 'data-input': 
-		// 			selectedNavItem = offset + 1
-		// 			break
-		// 		case 'record': 
-		// 			selectedNavItem = offset + 2
-		// 			break
-		// 		case 'user':
-		// 			selectedNavItem = offset + 3
-		// 			break
-		// 		default:
-		// 			selectedNavItem = offset
-		// 	}
-
-		// 	if (pathname.indexOf('collection') >= 0) {
-		// 		selectedNavItem = Number(query) - 1
-		// 	}
-
-		// 	this.setState({ selectedNavItem })
-		// }
 	}
 
 	componentDidUpdate(prevProps) {
@@ -91,7 +41,10 @@ class Sidenav extends Component {
 
 	componentDidMount() {
 		// materialize css initialization
-		// M.AutoInit()
+		this.initMaterialize()
+	}
+
+	initMaterialize() {
 		let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
 
@@ -113,27 +66,8 @@ class Sidenav extends Component {
 		const { selectedNavItem } = this.state
 		const { 
 			user,
-			// defaultNavItem, 
-			// collectionNavItem, 
-			// userGroupLinkAccess, 
-			// currentUserGroup,
-			// collectionNavItemLinks,
 			groupLinks
 		} = this.props;
-
-		// const shownDefaultNavItemLinks = defaultNavItem ? defaultNavItem.links : []
-		// const shownCollectionNavItemLinks = user.isLoggedIn && collectionNavItem ? collectionNavItem.links : []
-		// const shownCollectionNavItemLinks = user.isLoggedIn ? collectionNavItemLinks : []
-
-		// show menu based on user group authorization
-		// const shownDefaultNavItemLinks = 
-		// 	defaultNavItem.links.filter(item => 
-		// 		userGroupLinkAccess[currentUserGroup].indexOf(item.name) >= 0)
-		
-			// collectionNavItem.links.filter(item => 
-				// userGroupLinkAccess[currentUserGroup].indexOf(item.name) >= 0)
-		
-		// const offset = shownCollectionNavItemLinks ? shownCollectionNavItemLinks.length : 0
 
 		return (
 			<Fragment>
@@ -141,54 +75,6 @@ class Sidenav extends Component {
 				<a data-target="slide-out" className="sidenav-trigger"><i className="material-icons">menu</i></a>
 
 				<ul id="slide-out" className="sidenav">
-			    {
-			    	// collectionNavItem && collectionNavItem.header && collectionNavItem.header.length > 0 &&
-			    	// <li><a className="subheader">{collectionNavItem.header}</a></li>
-			    }
-			    {
-			    	// shownCollectionNavItemLinks && shownCollectionNavItemLinks.map((item, i) => (
-			    	// 	<div key={i} >
-				    // 		<div className={selectedNavItem === i ? 'active' : ''} onClick={this.handleClickNavItem.bind(this, i)}>
-					   //  		<li>
-							 //    	{
-					   //  				item.isExternal ?
-								// 	    	<a href={item.route}>
-								// 	    		<i className="material-icons">{item.icon}</i>
-								// 	    		{item.text}
-								// 	    	</a>
-								// 	    : <Link to={item.route}>
-								// 	    		<i className="material-icons">{item.icon}</i>
-								// 	    		{item.text}
-								// 	    	</Link>
-					   //  			}
-							 //    </li>
-							 //  </div>
-						  //   {
-						  //   	item.sublink &&
-					   //  		item.sublink.length > 0 && 
-					   //  		item.sublink.map((subitem, idx) => (
-					   //  			<li key={idx} className="sublink">
-								//     	{
-						  //   				subitem.isExternal ?
-								// 		    	<a href={subitem.route}>
-								// 		    		<i className="material-icons">{subitem.icon}</i>
-								// 		    		{subitem.text}
-								// 		    	</a>
-								// 		    : <Link to={subitem.route}>
-								// 		    		<i className="material-icons">{subitem.icon}</i>
-								// 		    		{subitem.text}
-								// 		    	</Link>
-						  //   			}
-								//     </li>
-					   //  		))
-					   //  	}
-						  // </div>
-			    	// ))
-			  	}
-			  	{
-			  		// collectionNavItem && collectionNavItem.dividerBottom &&
-			  		// <li><div className="divider"></div></li>
-			  	}
 			  	{
 			  		groupLinks &&
 			  		groupLinks.map((groupLink, index) => (
@@ -257,39 +143,6 @@ class Sidenav extends Component {
 			  			</div>
 			  		))
 			  	}
-			  	{
-			  		// defaultNavItem &&
-			  		// defaultNavItem.header &&
-			    // 	defaultNavItem.header.length > 0 &&
-			    // 	<li><a className="subheader">{defaultNavItem.header}</a></li>
-			    }
-			    {
-			    	// shownDefaultNavItemLinks.map((item, i) => (
-			    	// 	<div key={i} className={selectedNavItem === offset + i ? 'active' : ''} onClick={this.handleClickNavItem.bind(this, offset + i)}>
-				    // 		<li>
-						    	// {
-						    	// 	user.isLoggedIn && 
-						    	// 	<Link to={item.route}>
-							    // 		<i className="material-icons">{item.icon}</i>
-							    // 		{item.text}
-							    // 	</Link>
-						    	// }
-						    	// {
-						    	// 	!user.isLoggedIn && 
-						    	// 	<a className="modal-trigger" href="#modal-login-message">
-							    // 		<i className="material-icons">{item.icon}</i>
-							    // 		{item.text}
-							    // 	</a>
-						    	// }
-						  //   </li>
-						  // </div>
-			    	// ))
-			  	}
-			  	{
-			  		// defaultNavItem &&
-			  		// defaultNavItem.dividerBottom &&
-			  		// <li><div className="divider"></div></li>
-			  	}
 			  </ul>
 
 			  <div id="modal-login-message" className="modal">
@@ -303,56 +156,10 @@ class Sidenav extends Component {
 	}
 }
 
-Sidenav.defaultProps = {
-	// defaultNavItem: {
-	// 	header: 'Setup',
-	// 	links: [
-	// 		{	name: 'collection-list',
-	// 			route: '/collection-list',
-	// 			icon: 'apps',
-	// 			text: 'Collections',
-	// 		},
-	// 		{	name: 'sidenav-setup',
-	// 			route: '/sidenav-setup',
-	// 			icon: 'settings',
-	// 			text: 'Sidenav',
-	// 		},
-	// 	],
-	// 	dividerBottom: false,
-	// },
-
-	// options of current user group links authorization
-	// menu in sidenav will be filtered based on the specified menu name
-	// in the strings array
-	// userGroupLinkAccess:  {
-	// 	admin: [
-	// 		'dashboard',
-	// 		'form-list',
-	// 		'record', 
-	// 		'user',
-	// 	],
-		
-	// 	premiumUser: [
-	// 		'dashboard',
-	// 		'record',
-	// 		'user', 
-	// 		'collection1',
-	// 		'collection3',
-	// 	]
-	// },
-
-	// current group that the user belongs
-	// test: change below value to be: 'admin' or 'premiumUser'
-	// currentUserGroup: 'admin'
-}
-
 const mapStateToProps = (state) => {
   return {
     user: state.user,
   	appName: state.user.appName,
-    collectionNavItemLinks: state.user ? state.user.collectionNavItemLinks : [],
-    collectionNavItem: state.user.collectionNavItem,
-    defaultNavItem: state.user.defaultNavItem,
     groupLinks: state.user.sidenavGroupLinks ? state.user.sidenavGroupLinks : [],
     sidenavConfig: state.user.sidenavConfig,
   }
@@ -363,10 +170,6 @@ const mapDispatchToProps = (dispatch) => {
 		setApp: (appName) => dispatch(ACT.setApp(appName)),
 		loadSidenavConfig: (appName) => dispatch(ACT.loadSidenavConfig(appName)),
 		setSidenavFromConfig: (collections, groupLinks) => dispatch(ACT.setSidenavFromConfig(collections, groupLinks)),
-		setDefaultNavItem: () => dispatch(ACT.setDefaultNavItem()),
-		setCollectionNavItem: () => dispatch(ACT.setCollectionNavItem()),
-		setSidenavGroupLinks: (groupLinks) => dispatch(ACT.setSidenavGroupLinks(groupLinks)),
-		loadCollectionNavItemLinks: () => dispatch(ACT.loadCollectionNavItemLinks())
 	}
 }
 
