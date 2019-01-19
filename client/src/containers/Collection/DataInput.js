08bc42ca1136daae86e3becb8abc321df23ea94d
Iqbal Novramadani <iqbal.slf@gmail.com>
2019-01-19 21:45:06 +0700

    refactor: move arrayFieldTemplate for jsonSchema form to utils folder

--- client/src/containers/Collection/DataInput.js
+++ client/src/containers/Collection/DataInput.js
@@ -4,6 +4,7 @@ import Form from 'react-jsonschema-form'
 import M from 'materialize-css/dist/js/materialize.min.js'
 
 import API_URL from '../../utils/api_url'
+import { arrayFieldTemplate } from '../../utils/jsonSchemaFormUITemplate'
 import './DataInput.css'
 
 class DataInput extends Component {
@@ -26,7 +27,7 @@ class DataInput extends Component {
 						schema={formStructure}
 						uiSchema={uiSchema}
 	        	onSubmit={this.onSubmit.bind(this)}
-	        	ArrayFieldTemplate={this.arrayFieldTemplate}
+	        	ArrayFieldTemplate={arrayFieldTemplate}
 	        	onError={this.log("errors")} />
 	      </div>
 			</div>
@@ -74,90 +75,6 @@ class DataInput extends Component {
 			})
 			.catch(err => console.log(err))
 	}
-
-	arrayFieldTemplate = (props) => {
-		const innerObjectProperties = props.items[0] ? props.items[0].children.props.schema.properties : {}
-	  return (
-	    <div>
-	    	<div className="row">
-		    	<div className="col s11">
-			    	<table>
-							<thead>
-								<tr className="array-item-table-head">
-								{
-									Object.keys(innerObjectProperties).map((property, index) => (
-										<th key={index}>{innerObjectProperties[property].title}</th>
-									))
-								}
-								</tr>
-							</thead>
-						</table>
-					</div>
-				</div>
-	      {
-	      	props.items.map((element, idx1) => {
-	      		element.children.props.registry.ObjectFieldTemplate = innerObjectFieldTemplate
-	      		return (
-	      			<div className="row zero-margin">
-		      			<div className="col s11">{element.children}</div>
-		      			<div className="btn-actions-container">
-			            {element.hasMoveDown && (
-			              <span
-			              	className="waves-effect waves-light btn btn-action-array-field"
-			                onClick={element.onReorderClick(
-			                  element.index,
-			                  element.index + 1
-			                )}>
-			                <i className="material-icons">arrow_drop_down</i>
-			              </span>
-			            )}
-			            {element.hasMoveUp && (
-			              <span
-			              	className="waves-effect waves-light btn btn-action-array-field"
-			                onClick={element.onReorderClick(
-			                  element.index,
-			                  element.index - 1
-			                )}>
-			                <i className="material-icons">arrow_drop_up</i>
-			              </span>
-			            )}
-			            {element.hasRemove && (
-			            	<span 
-				            	className="waves-effect waves-light btn btn-action-array-field"
-				            	onClick={element.onDropIndexClick(element.index)}>
-				              <i className="material-icons">delete</i>
-				            </span>
-			            )}			            
-			          </div>
-	            </div>
-	      		)
-	      	})
-	      }
-	      {
-	      	props.canAdd && 
-	      	<span className="waves-effect waves-light btn btn-floating red right" onClick={props.onAddClick}>
-	      		<i className="material-icons">add</i>
-	      	</span>
-	      }
-	    </div>
-	  )
-	}
-}
-
-const innerObjectFieldTemplate = (props) => {
-	return (
-		<table>
-		<tbody>
-			<tr className="array-object-properties-row">
-			{
-				props.properties.map((property, idx2) => (
-					<td key={idx2}>{property.content}</td>
-				))
-			}
-			</tr>
-		</tbody>
-		</table>
-	)
 }
 
 export default DataInput
---
+++ client/src/utils/jsonSchemaFormUITemplate.js
@@ -0,0 +1,85 @@
+import React from 'react'
+
+export const arrayFieldTemplate = (props) => {
+	const innerObjectProperties = props.items[0] ? props.items[0].children.props.schema.properties : {}
+  return (
+    <div>
+    	<div className="row">
+	    	<div className="col s11">
+		    	<table>
+						<thead>
+							<tr className="array-item-table-head">
+							{
+								Object.keys(innerObjectProperties).map((property, index) => (
+									<th key={index}>{innerObjectProperties[property].title}</th>
+								))
+							}
+							</tr>
+						</thead>
+					</table>
+				</div>
+			</div>
+      {
+      	props.items.map((element, idx1) => {
+      		element.children.props.registry.ObjectFieldTemplate = innerObjectFieldTemplate
+      		return (
+      			<div className="row zero-margin">
+	      			<div className="col s11">{element.children}</div>
+	      			<div className="btn-actions-container">
+		            {element.hasMoveDown && (
+		              <span
+		              	className="waves-effect waves-light btn btn-action-array-field"
+		                onClick={element.onReorderClick(
+		                  element.index,
+		                  element.index + 1
+		                )}>
+		                <i className="material-icons">arrow_drop_down</i>
+		              </span>
+		            )}
+		            {element.hasMoveUp && (
+		              <span
+		              	className="waves-effect waves-light btn btn-action-array-field"
+		                onClick={element.onReorderClick(
+		                  element.index,
+		                  element.index - 1
+		                )}>
+		                <i className="material-icons">arrow_drop_up</i>
+		              </span>
+		            )}
+		            {element.hasRemove && (
+		            	<span 
+			            	className="waves-effect waves-light btn btn-action-array-field"
+			            	onClick={element.onDropIndexClick(element.index)}>
+			              <i className="material-icons">delete</i>
+			            </span>
+		            )}			            
+		          </div>
+            </div>
+      		)
+      	})
+      }
+      {
+      	props.canAdd && 
+      	<span className="waves-effect waves-light btn btn-floating red right" onClick={props.onAddClick}>
+      		<i className="material-icons">add</i>
+      	</span>
+      }
+    </div>
+  )
+}
+
+const innerObjectFieldTemplate = (props) => {
+	return (
+		<table>
+		<tbody>
+			<tr className="array-object-properties-row">
+			{
+				props.properties.map((property, idx2) => (
+					<td key={idx2}>{property.content}</td>
+				))
+			}
+			</tr>
+		</tbody>
+		</table>
+	)
+}
