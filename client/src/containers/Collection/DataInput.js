6d0cd309d7fcd09dbf493559e61078d27062d105
Iqbal Novramadani <iqbal.slf@gmail.com>
2019-01-19 12:44:41 +0700

    feat: render array field like table-view inside form

--- client/src/containers/Collection/DataInput.js
+++ client/src/containers/Collection/DataInput.js
@@ -17,7 +17,6 @@ class DataInput extends Component {
 
 	render() {
 		const { formStructure } = this.state
-
 		return (
 			<div className="form-input">
 				<h5>Input form</h5>
@@ -25,6 +24,7 @@ class DataInput extends Component {
 					<Form 
 						schema={formStructure}
 	        	onSubmit={this.onSubmit.bind(this)}
+	        	ArrayFieldTemplate={this.arrayFieldTemplate}
 	        	onError={this.log("errors")} />
 	      </div>
 			</div>
@@ -71,6 +71,91 @@ class DataInput extends Component {
 			})
 			.catch(err => console.log(err))
 	}
+
+	arrayFieldTemplate = (props) => {
+		const innerObjectProperties = props.items[0] ? props.items[0].children.props.schema.properties : {}
+	  return (
+	    <div>
+	    	<fieldset>
+	    	<legend className="array-field-title">{props.schema.title}</legend>
+	    	<div className="row">
+		    	<div className="col s11">
+			    	<table>
+							<thead>
+								<tr className="array-item-table-head">
+								{
+									Object.keys(innerObjectProperties).map((property, index) => (
+										<th key={index}>{innerObjectProperties[property].title}</th>
+									))
+								}
+								</tr>
+							</thead>
+						</table>
+					</div>
+				</div>
+	      {
+	      	props.items.map((element, idx1) => {
+	      		element.children.props.registry.ObjectFieldTemplate = innerObjectFieldTemplate
+	      		return (
+	      			<div className="row zero-margin">
+		      			<div className="col s11">{element.children}</div>
+		      			<div className="btn-actions-container">
+			            {element.hasMoveDown && (
+			              <span
+			              	className="waves-effect waves-light btn btn-action-array-field"
+			                onClick={element.onReorderClick(
+			                  element.index,
+			                  element.index + 1
+			                )}>
+			                <i className="material-icons">arrow_drop_down</i>
+			              </span>
+			            )}
+			            {element.hasMoveUp && (
+			              <span
+			              	className="waves-effect waves-light btn btn-action-array-field"
+			                onClick={element.onReorderClick(
+			                  element.index,
+			                  element.index - 1
+			                )}>
+			                <i className="material-icons">arrow_drop_up</i>
+			              </span>
+			            )}
+			            <span 
+			            	className="waves-effect waves-light btn btn-action-array-field"
+			            	onClick={element.onDropIndexClick(element.index)}>
+			              <i className="material-icons">delete</i>
+			            </span>
+			          </div>
+	            </div>
+	      		)
+	      	})
+	      }
+	      {
+	      	props.canAdd && 
+	      	<span className="waves-effect waves-light btn btn-floating red right" onClick={props.onAddClick}>
+	      		<i className="material-icons">add</i>
+	      	</span>
+	      }
+	      </fieldset>
+	    </div>
+	  )
+	}	
 }
 
-export default DataInput;
+const innerObjectFieldTemplate = (props) => {
+	return (
+		<table>
+		<tbody>
+			<tr className="array-object-properties-row">
+			{
+				props.properties.map((property, idx2) => (
+					<td key={idx2}>{property.content}</td>
+				))
+			}
+			</tr>
+		</tbody>
+		</table>
+	)
+}
+
+export default DataInput
