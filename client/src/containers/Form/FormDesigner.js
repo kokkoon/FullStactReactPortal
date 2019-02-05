20a1213e49233f648669bbde9ece344a889dd7ea
Iqbal Novramadani <iqbal.slf@gmail.com>
2019-02-05 17:41:02 +0700

    style: remove commented codes

--- client/src/containers/Form/FormDesigner.js
+++ client/src/containers/Form/FormDesigner.js
@@ -1,16 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-// import axios from 'axios'
-// import queryString from 'query-string'
-// import M from 'materialize-css/dist/js/materialize.min.js'
-// import Form from 'react-jsonschema-form'
-// import { isEqual } from 'lodash'
 
-// import { arrayFieldTemplate } from '../../utils/jsonSchemaFormUITemplate'
-// import customFields from '../../utils/RJSFCustomFields'
-// import * as helper from '../../utils/helperFunctions'
-// import API_URL from '../../utils/api_url'
-// import * as ACT from '../../actions'
 import './FormDesigner.css'
 
 class FormDesigner extends Component {
@@ -138,24 +128,6 @@ class FormDesigner extends Component {
 				</div>
 			</div>
 	  )
-	}
-
-	componentWillMount() {
-		this.loadData()
-	}
-
-	componentDidMount() {
-		// M.AutoInit()
-	}
-
-	loadData() {
-		// const { location } = this.props
-
-		// axios.get(`${API_URL}/form?id=${formId}`)
-		// 	.then(res => {
-				
-		// 	})
-		// 	.catch(e => console.error(e))
 	}
 
 	handleDropFormControl = (e) => {
